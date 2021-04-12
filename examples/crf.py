409a5593710f017f9caa0b70b16c037d39da07b1
18601995729@163.com <18601995729@163.com>
2021-04-12 20:54:35 +0800

    xxx

---
+++ examples/crf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 4/9/21 3:01 下午
+# @Author  : 4c1p
+# @Description: <>
+
+from typing import TypeVar, Any
+
+
+if __name__ == '__main__':
+    pass
