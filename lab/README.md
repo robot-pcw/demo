38d0532617434d081278d2b4acbfbf81b1039aef
18601995729@163.com <18601995729@163.com>
2021-04-15 21:46:46 +0800

    lab init

---
+++ lab/README.md
@@ -0,0 +1,3 @@
+* Q1: L1/L2 Reg visualization
+
+* Q2: decision boundary visualization
---
+++ lab/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2/17/21 1:34 下午
+# @Author  : 4c1p
+# @Description: <>
